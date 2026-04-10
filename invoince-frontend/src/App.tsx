88685c16b4175c0ccd00c2db390e181b26d5f715
Nie <22130317@st.hcmuaf.edu.vn>
2026-04-10 09:23:52 +0700

    vadkjgfra

--- invoince-frontend/src/App.tsx
+++ invoince-frontend/src/App.tsx
@@ -4,6 +4,7 @@ import ItemDetailsForm from './form_checkout/checkout';
 import PaymentQueue from './Confirm_Payment/confirm_payment';
 import RecordBill from './RecordBill/RecordBill';
 import Login from './Login/login'; 
+import Register from './register/regiter';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         <Route path="/" element={<Navigate to="/login" replace />} />
         
         <Route path="/login" element={<Login />} />
-
+        <Route path="/register" element={<Register/>} />
         <Route
           path="/*"
           element={
@@ -24,6 +25,7 @@ function App() {
                 <Route path="/payments" element={<PaymentQueue />} />
                 <Route path="/record-bill" element={<RecordBill />} />
                 <Route path="/dashboard" element={<div className="p-8">Dashboard Page</div>} />
+              
                 
                 <Route path="*" element={<div className="p-8">404 - Không tìm thấy trang</div>} />
               </Routes>
